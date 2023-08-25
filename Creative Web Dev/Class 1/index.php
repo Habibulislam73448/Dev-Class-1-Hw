8f03329eb4b6b91839ca1ad27c65a85c38552f97
Md Habibul Islam <135532621+Habibulislam73448@users.noreply.github.com>
2023-08-25 23:58:59 +0600

    Add files via upload

---
+++ Creative Web Dev/Class 1/index.php
@@ -0,0 +1,30 @@
+<?php
+// Print Statement
+
+echo "<h1> Hello World! </h1><br>";
+
+echo "<h3> Hey, This is Md Habibul Islam, Batch no IAPWD-2301.</h3>";
+echo "I'm completing my Industrial Attatchment from Creative It Institute on Web Development. It was my first HW of Raw PHP.<br> <br>";
+
+// Number
+    $a = 65;
+    $b = 35;
+    echo $a+$b;
+
+// Var Declaration
+    $user_name = "<h3>Md Habibul Islam</h3>";
+    $institute = "<h3> Chittagong Polytechnic Institute</h3>";
+
+    echo $user_name,$institute;
+
+// Concathenation
+    $name = "Habib";
+    $game = "Football.";
+    $position = "Goalkeeping";
+
+    echo $name.' '. 'loves to playing ' .$game; 
+    echo 'He is good in '.$position.' position. <br>';
+
+// Content Declaration
+    define("id", "01");
+    echo id;
